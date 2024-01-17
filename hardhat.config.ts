ed29eb2d68069d7162853e0f4565bd1d7b78d0e6
Slaweekq <plakidin.vyacheslav@mail.ru>
2024-01-17 17:56:58 +0700

    «new»

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,7 +22,7 @@ const { REPORT_GAS, TOKEN, GAS_PRICE_API, COINMARKETCAP_API_KEY } = envConfig.pa
 
 /** @type import('hardhat/config').HardhatUserConfig */
 const DEFAULT_COMPILER_SETTINGS: SolcUserConfig = {
-  version: '0.8.9',
+  version: '0.8.22',
   settings: {
     optimizer: {
       enabled: true,
@@ -34,7 +34,6 @@ const DEFAULT_COMPILER_SETTINGS: SolcUserConfig = {
     evmVersion: 'shanghai'
   }
 };
-
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   paths: {
@@ -64,7 +63,30 @@ const config: HardhatUserConfig = {
     customChains: customChains
   },
   solidity: {
-    compilers: [DEFAULT_COMPILER_SETTINGS]
+    compilers: [
+      DEFAULT_COMPILER_SETTINGS,
+      {
+        version: '0.8.16',
+        settings: {
+          optimizer: { enabled: true, runs: 1_000 },
+          metadata: { bytecodeHash: 'none' }
+        }
+      },
+      {
+        version: '0.8.0',
+        settings: {
+          optimizer: { enabled: true, runs: 1_000 },
+          metadata: { bytecodeHash: 'none' }
+        }
+      },
+      {
+        version: '0.6.0',
+        settings: {
+          optimizer: { enabled: true, runs: 1_000 },
+          metadata: { bytecodeHash: 'none' }
+        }
+      }
+    ]
   },
   contractSizer: {
     alphaSort: false,
@@ -75,15 +97,15 @@ const config: HardhatUserConfig = {
     deployer: 0,
     admin: 1
   },
-  abiExporter: {
-    path: './build/abis',
-    runOnCompile: true,
-    clear: true,
-    flat: true,
-    only: [],
-    spacing: 2,
-    pretty: true
-  },
+  // abiExporter: {
+  //   path: './build/abis',
+  //   runOnCompile: true,
+  //   clear: true,
+  //   flat: true,
+  //   only: [],
+  //   spacing: 2,
+  //   pretty: true
+  // },
   gasReporter: {
     enabled: REPORT_GAS === ('true' || true) ? true : false,
     noColors: true,
--- interfaces/IUniswapDex.sol
+++ interfaces/IUniswapDex.sol
@@ -4,15 +4,12 @@ pragma solidity ^0.8.10;
 
 interface IFactory {
     function createPair(address tokenA, address tokenB) external returns (address pair);
-
     function getPair(address tokenA, address tokenB) external view returns (address pair);
 }
 
 interface IRouter {
     function factory() external pure returns (address);
-
     function WETH() external pure returns (address);
-
     function addLiquidityETH(
         address token,
         uint256 amountTokenDesired,
@@ -21,7 +18,6 @@ interface IRouter {
         address to,
         uint256 deadline
     ) external payable returns (uint256 amountToken, uint256 amountETH, uint256 liquidity);
-
     function swapExactTokensForETHSupportingFeeOnTransferTokens(
         uint256 amountIn,
         uint256 amountOutMin,
@@ -29,7 +25,6 @@ interface IRouter {
         address to,
         uint256 deadline
     ) external;
-
     function swapExactETHForTokensSupportingFeeOnTransferTokens(
         uint256 amountOutMin,
         address[] calldata path,
